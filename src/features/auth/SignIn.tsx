bab5d03d18d6ab70d26224c36d112493c6522fb2
jos3lo89 <ganbaru743@gmail.com>
2025-12-04 17:21:16 -0500

    feat: implement initial sign-in page

---
+++ src/features/auth/SignIn.tsx
@@ -0,0 +1,10 @@
+import SignInForm from "./components/SignInForm";
+
+const SignIn = () => {
+  return (
+    <div className="flex items-center justify-center mt-10 bg-background">
+      <SignInForm />
+    </div>
+  );
+};
+export default SignIn;
